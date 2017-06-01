2971e58f0e48dbd8c697a39774ea2afda6a74dfd
Simon Damminga <99039973@mydavinci.nl>
2017-06-01 09:15:42 +0200

    no message

--- core/config.php
+++ core/config.php
@@ -4,7 +4,7 @@ define('DB_TYPE', 'mysql');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'hospital');
 define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_PASS', 'C#klh?!879');
 define('DB_CHARSET', 'utf8');
 
 define('URL_PUBLIC_FOLDER', 'public'); 
